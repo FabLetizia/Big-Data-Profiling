f5c2653a779332f34e3f8ee3a4665dde8f0073c1
Elisa Catena <elisacatena00@gmail.com>
2024-06-25 15:13:25 +0200

    Update YdataGZ.py

--- YdataGZ.py
+++ YdataGZ.py
@@ -15,19 +15,16 @@ logging.basicConfig(level=logging.INFO)
 parser = argparse.ArgumentParser()
 # input and output paths passed from the command line
 parser.add_argument("--input_path", type=str, help="Input dataset path")
-parser.add_argument("--output_path", type=str, help="Output folder path")
  
 args = parser.parse_args()
-dataset_filepath, output_filepath = args.input_path, args.output_path
+dataset_filepath = args.input_path
 
 spark_session = (
 SparkSession.builder.appName("SparkProfiling").master("local[*]").config("spark.executor.memory", "4g").config("spark.driver.memory", "4g").getOrCreate()
 )
 
-
 # Carica il dataset in un DataFrame Spark
 spark_df = spark_session.read.csv(dataset_filepath, header=True)
-
 
 cfg = Settings()
 cfg.infer_dtypes = False
@@ -47,4 +44,4 @@ warnings.filterwarnings("ignore", category=MatplotlibDeprecationWarning)
 profile = ProfileReport(spark_df, config=cfg)
 
 # Genera e salva il report di profilazione
-profile.to_file("/Users/elisacatena/Desktop/song_lyrics_report.html")
+profile.to_file("song_lyrics_report.html")

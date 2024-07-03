55149aeb25cdf106a1d75d880ffd0feaf6baf985
FabLetizia <fabio.letizia01@gmail.com>
2024-07-03 16:54:16 +0200

    fix ydata

--- src/data profiling/Ydata.py
+++ src/data profiling/Ydata.py
@@ -1,16 +1,15 @@
 import logging
 import warnings
-
 from matplotlib import MatplotlibDeprecationWarning
 from pyspark.sql import SparkSession
-
 from ydata_profiling import ProfileReport
 from ydata_profiling.config import Settings
-
 import argparse
 
-
 logging.basicConfig(level=logging.INFO)
+
+''' This Spark core script in python performs fdata profiling on the input csv taken from a
+    hdfs path and generates a report file with statistics in html format '''
 
 parser = argparse.ArgumentParser()
 # input and output paths passed from the command line
@@ -23,9 +22,9 @@ spark_session = (
 SparkSession.builder.appName("SparkProfiling").master("local[*]").config("spark.executor.memory", "4g").config("spark.driver.memory", "4g").getOrCreate()
 )
 
-# Carica il dataset in un DataFrame Spark
 spark_df = spark_session.read.csv(dataset_filepath, header=True, inferSchema=True)
 
+# DA LEVARE
 cfg = Settings()
 cfg.infer_dtypes = False
 cfg.correlations["auto"].calculate = False
@@ -41,7 +40,7 @@ cfg.samples.random = 0
 # Create and start the monitoring process
 warnings.filterwarnings("ignore", category=MatplotlibDeprecationWarning)
 
+# ydata_profiling tool
 profile = ProfileReport(spark_df, config=cfg)
 
-# Genera e salva il report di profilazione
-profile.to_file("song_lyrics_report.html")
+profile.to_file("report.html")
